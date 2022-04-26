4a0d507a2757094c873bf60e1b238037da9d7078
Wyatt Kribs <kribswyatt@gmail.com>
2022-04-26 17:23:58 -0600

    refactor methods

--- lib/translator.rb
+++ lib/translator.rb
@@ -24,14 +24,33 @@ class Translator
 
   def translate
     braille_message = []
+    finished_message = ''
     read_input.split('').map do |character|
       @dictionary.braille_hash.map do |letter|
-        # require 'pry' ; binding.pry
         if letter[0] == character
           braille_message << letter[1]
         end
       end
     end
-    braille_message.flatten
+    line1 = ''
+    line2 = ''
+    line3 = ''
+    braille_message.each do |letter|
+      line1 += letter[0]
+      line2 += letter[1]
+      line3 += letter[2]
+    end
+    finished_message = line1 + "\n" + line2 + "\n" + line3
+    # require 'pry' ; binding.pry
   end
+
+  def read_output
+    File.read(@output)
+  end
+
+  def write
+    File.write(@output, translate)
+  end
+
+
 end
