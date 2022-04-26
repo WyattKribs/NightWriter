859a89f2e4b5baa3cc65ab741f59dd58b8683fec
Wyatt Kribs <kribswyatt@gmail.com>
2022-04-26 14:11:57 -0600

    add translator object and spec

---
+++ spec/translator_spec.rb
@@ -0,0 +1,12 @@
+require './lib/translator'
+
+describe Translator do
+  before(:each) do
+    @translator = Translator.new
+  end
+
+  it "exists" do
+    expect(@translator).to be_a(Translator)
+  end
+
+end
